8cba9756ec2970c73f233be24195106df24733c2
iamrosada <luisrosada2018@gmail.com>
2022-07-12 20:54:54 +0300

    feature: created the new component, container

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <template>
-  <Tutorial />
+  <ContainerComponent/>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import {ContainerComponent} from '../components/ContainerComponent/container'
+import  '../assets/css/main.css'
 
 export default Vue.extend({
-  name: 'IndexPage',
+ components:{
+  ContainerComponent
+ }
 })
 </script>
